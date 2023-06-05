c66480c5b3e25a0a5db0b245a9760d83ba5bf4c0
Bilquise <bilquiseather@jtechworld.com>
2023-06-05 18:07:57 +0530

    tic-tac-toe game 🌟

---
+++ src/components/Box.jsx
@@ -0,0 +1,13 @@
+import './Box.css';
+
+const Box = ({ value, onClick }) => {
+  const style = value === 'X' ? 'box X' : 'box O';
+
+  return (
+    <button className={style} onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Box;
